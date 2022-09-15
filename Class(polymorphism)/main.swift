329bf102284d2271b99d91dbdfcd4969beecabc5
Boris Z <Boris Z>
2022-09-15 16:59:56 +0300

    !

--- Class(polymorphism)/main.swift
+++ Class(polymorphism)/main.swift
@@ -7,5 +7,47 @@
 
 import Foundation
 
-print("Hello, World!")
+class Shape {
+    func draw() {
+        print("Draw shape")
+    }
+}
+
+class Rectangle: Shape {
+    override func draw() {
+        super.draw()
+        print("Draw rectangle")
+    }
+}
+
+class Circle: Shape {
+    override func draw() {
+        print("Draw circle")
+    }
+}
+
+//func drawRectangle(_ rectangle: Rectangle) {
+//    rectangle.draw()
+//}
+//
+//func drawCircle(_ circle: Circle) {
+//    circle.draw()
+//}
+
+func drawShape(_ shape: Shape) {
+    shape.draw()
+}
+
+
+let rectangle = Rectangle()
+let circle = Circle()
+let shape = Shape()
+
+drawShape(shape)
+drawShape(rectangle)
+drawShape(circle)
+//drawCircle(circle)
+//drawRectangle(rectangle)
+
+
 
